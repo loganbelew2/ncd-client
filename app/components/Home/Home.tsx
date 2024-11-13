5eec9165667c09c30d8587a95cc0783c6f524cb9
Logan Belews <Loganbelew2@gmail.com>
2024-11-12 23:55:12 -0600

    gift cards galore

--- app/components/Home/Home.tsx
+++ app/components/Home/Home.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useEffect } from "react";
 import Image from "next/image";
 import styles from "./Home.module.css";
 import PrimaryButton from "../Buttons/Primary-Button";
@@ -11,7 +12,33 @@ import AboutUs from "../AboutUs/AboutUs";
 import WhyUs from "../WhyUs/WhyUs";
 import { Packages } from "../Packages/Packages";
 import LocationsLearn from "../Locations/LocationsLearn/LocationsLearn";
+
 export default function Home() {
+  useEffect(() => {
+    (function (g: any, i: Document, f: string, t: string, u: string) {
+      g[u] =
+        g[u] ||
+        function () {
+          (g[u].q = g[u].q || []).push(arguments);
+        };
+
+      const p = i.createElement(f) as HTMLScriptElement;
+      p.async = true;
+      p.src = t;
+
+      const s = i.getElementsByTagName(f)[0];
+      if (s && s.parentNode) {
+        s.parentNode.insertBefore(p, s);
+      }
+    })(
+      window,
+      document,
+      "script",
+      "https://cdn.giftup.app/dist/gift-up.js",
+      "giftup"
+    );
+  }, []);
+
   return (
     <div className={styles.home__container}>
       <section className={styles.hero__container}>
@@ -49,11 +76,26 @@ export default function Home() {
           <SecondaryButton />
         </div>
       </section>
+
       <WhyUs />
       <Services />
       <Process />
       <Packages />
       <LocationsLearn />
+
+      {/* Gift Up widget */}
+      <section className={styles.giftWrapper}>
+        <h2>Feeling Generous? Give Someone A Gift Card </h2>
+        <p>
+          Perfect for birthdays, holidays or celebrations! Get it delivered by
+          email or print it out at home.
+        </p>
+        <div
+          className="gift-up-target"
+          data-site-id="1b1388e7-e3dd-413c-a8df-08dd032eab51"
+          data-platform="Other"
+        ></div>
+      </section>
       <AboutUs />
     </div>
   );
