f4c18dc2febe096b36022858e22c5b51f0ec0369
Logan Belews <Loganbelew2@gmail.com>
2025-01-08 15:12:13 -0600

    closed

--- app/components/Booking/Bookingv2.tsx
+++ app/components/Booking/Bookingv2.tsx
@@ -1,27 +1,32 @@
 "use client";
 import { useEffect } from "react";
 export default function BookingV2() {
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src =
-      "https://squareup.com/appointments/buyer/widget/3inp0q2s30l40e/LGS9SEJZ0MAGP.js";
-    script.async = true;
-    script.id = "square-appointments-script";
+  // useEffect(() => {
+  //   const script = document.createElement("script");
+  //   script.src =
+  //     "https://squareup.com/appointments/buyer/widget/3inp0q2s30l40e/LGS9SEJZ0MAGP.js";
+  //   script.async = true;
+  //   script.id = "square-appointments-script";
 
-    document
-      .getElementById("square-appointments-container")
-      ?.appendChild(script);
+  //   document
+  //     .getElementById("square-appointments-container")
+  //     ?.appendChild(script);
 
-    return () => {
-      // Cleanup to remove the script when the component unmounts
-      const existingScript = document.getElementById(
-        "square-appointments-script"
-      );
-      if (existingScript) {
-        existingScript.remove();
-      }
-    };
-  }, []);
+  //   return () => {
+  //     // Cleanup to remove the script when the component unmounts
+  //     const existingScript = document.getElementById(
+  //       "square-appointments-script"
+  //     );
+  //     if (existingScript) {
+  //       existingScript.remove();
+  //     }
+  //   };
+  // }, []);
 
-  return <div id="square-appointments-container"></div>;
+  // return <div id="square-appointments-container"></div>;
+  return (
+    <>
+    <h1>Sorry we are temporarily closed.</h1>
+    </>
+  )
 }
